56899a53009bdff5da04722e609cc6e6ddd5be6c
Ruben Jonatan Maza Vilchez <rmazavil@emeal.nttdata.com>
2022-12-01 16:07:32 -0500

    update

--- src/main/java/com/nttdata/bootcamp/BootcampGatewayApplication.java
+++ src/main/java/com/nttdata/bootcamp/BootcampGatewayApplication.java
@@ -32,8 +32,8 @@ public class BootcampGatewayApplication {
 		List<GroupedOpenApi> groups = new ArrayList<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
 		assert definitions != null;
-		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".bootcamp-*")).forEach(routeDefinition -> {
-			String name = routeDefinition.getId().replaceAll("bootcamp-", "");
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("", "");
 			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
 		});
 		return groups;
