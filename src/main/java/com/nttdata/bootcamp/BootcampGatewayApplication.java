2172f0ce5425b8b7a6d0c126c54fd2ee13a46882
Ruben Jonatan Maza Vilchez <rmazavil@emeal.nttdata.com>
2022-12-01 14:47:52 -0500

    update

--- src/main/java/com/nttdata/bootcamp/BootcampGatewayApplication.java
+++ src/main/java/com/nttdata/bootcamp/BootcampGatewayApplication.java
@@ -1,15 +1,42 @@
 package com.nttdata.bootcamp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class BootcampGatewayApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BootcampGatewayApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampGatewayApplication.class, args);
+	}
+
+	@Autowired
+	RouteDefinitionLocator locator;
+
+	@Bean
+	public List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".bootcamp-*")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("bootcamp-", "");
+			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+		});
+		return groups;
 	}
 
 }
